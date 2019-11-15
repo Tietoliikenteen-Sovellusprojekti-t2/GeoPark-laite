f3e984d35161064bce968ca4988e52e504968f72
SusiKun <susikun@icloud.com>
2019-11-15 14:05:26 +0200

    VideoWrap testikoodi

---
+++ koodit/Testikoodit/video_wrap.py
@@ -0,0 +1,11 @@
+import picamera
+
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+camera.start_recording('my_video.h264')
+camera.wait_recording(10)
+camera.stop_recording()
+
+command = "MP4Box -add my_video.h264 my_video.mp4"
+call([command], shell=True)
+print("vid conv")
