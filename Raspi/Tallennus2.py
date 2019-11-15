827c5b67469612bbe3a3fdb86cb51abe22a21072
t7mato01 <t7mato01@students.oamk.fi>
2019-11-15 12:25:55 +0200

    Lisättiin 2. kokeiluversio

--- Raspi/Tallennus2.py
+++ Raspi/Tallennus2.py
@@ -19,8 +19,8 @@ try:
         report = gpsd.next() #
         if report['class'] == 'TPV':
             GPStime =  str(getattr(report,'time',''))
-            lat = str(getattr(report,'lat',0.0))
-            lon = str(getattr(report,'lon',0.0))
+            lat = str(report) #str(getattr(report,'lat',0.0))
+            lon = str(report) #str(getattr(report,'lon',0.0))
 
             print  GPStime,"\t",
             print  lat,"\t",
