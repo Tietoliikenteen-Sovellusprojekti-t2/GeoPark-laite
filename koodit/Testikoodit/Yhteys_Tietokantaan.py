460d6c1715a30041061f142711fa3e3fad39e679
t7mato01 <t7mato01@students.oamk.fi>
2019-11-21 10:23:24 +0200

    Yhteys tietokantaan testi scipti

---
+++ koodit/Testikoodit/Yhteys_Tietokantaan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import MySQLdb #https://www.tutorialspoint.com/python/python_database_access.htm
+
+db=MySQLdb.connect("stulinux52.ipt.oamk.fi","ubuntu","antenni2","GeoPark")
+
+cursor = db.cursor()
+cursor.execute("SELECT VERSION()") #tahan niitä mysql komentoja
+
+data = cursor.fetchone()
+
+print data
+
+db.close()
