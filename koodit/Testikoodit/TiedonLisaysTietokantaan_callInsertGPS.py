5945ffb1c6de2cdb63c19d44534ea0a619f35980
SusiKun <susikun@icloud.com>
2019-11-21 12:36:38 +0200

    Lisätty SQL-skriptit ja päivitetty tietokanta

---
+++ koodit/Testikoodit/TiedonLisaysTietokantaan_callInsertGPS.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from gps import *
+import time, inspect, os
+import MySQLdb #https://www.tutorialspoint.com/python/python_database_access.htm
+
+db=MySQLdb.connect("stulinux52.ipt.oamk.fi","ubuntu","antenni2","GeoPark")
+gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
+try:
+    
+    while True:
+        report = gpsd.next() #
+        if report['class'] == 'TPV':
+            lat = str(getattr(report,'lat',0.0))
+            lon = str(getattr(report,'lon',0.0))
+            aika = time.strftime("%Y%m%d-%H%M%S")
+            cursor = db.cursor()
+
+            sql = "call Insert_GPS('lat', 'lon', 'aika';"
+
+            print ("Testi Testi")
+
+            cursor.execute(sql)
+            break
+
+db.close()
