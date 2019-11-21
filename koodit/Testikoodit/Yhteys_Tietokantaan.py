792a25d360010cafd7bf4524a31e1b01be53e11d
t7mato01 <t7mato01@students.oamk.fi>
2019-11-21 10:27:28 +0200

    poistettiin ääkköset...

--- koodit/Testikoodit/Yhteys_Tietokantaan.py
+++ koodit/Testikoodit/Yhteys_Tietokantaan.py
@@ -5,7 +5,7 @@ import MySQLdb #https://www.tutorialspoint.com/python/python_database_access.htm
 db=MySQLdb.connect("stulinux52.ipt.oamk.fi","ubuntu","antenni2","GeoPark")
 
 cursor = db.cursor()
-cursor.execute("SELECT VERSION()") #tahan niitä mysql komentoja
+cursor.execute("SELECT VERSION()") #tahan niita mysql komentoja
 
 data = cursor.fetchone()
 
