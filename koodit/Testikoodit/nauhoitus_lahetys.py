7ea438a02ef60f24f1deff7efc32e58cc4e85b5d
SusiKun <susikun@icloud.com>
2019-11-29 10:53:29 +0200

    Muoks

--- koodit/Testikoodit/nauhoitus_lahetys.py
+++ koodit/Testikoodit/nauhoitus_lahetys.py
@@ -16,13 +16,13 @@ video_form = ".mp4"
 video_koko = polku_local + video_nimi + video_form
 testi4 = "30"
 
-def nimiMuunnin (video_nro, video_koko, video_nimi, video_form, polku_local):
-    while os.path.isfile(video_koko):
-        video_nimi = "my_video%d" %(video_nro)
-        video_nro += 1
-        video_koko = polku_local + video_nimi + video_form
+def nimiMuunnin (video_nro, video_koko, video_nimi, video_form, polku_local): #Funktio videon nimemämistä varten
+    while os.path.isfile(video_koko):                                         #Tarkistaa onko kyseisellä nimellä olevaa videotiedostoa olemassa                
+        video_nimi = "my_video%d" %(video_nro)                                #muodostetaan videon nimi uudestaan
+        video_nro += 1                                                        #Nostetaan videon numeromuuttujaa yhdellä
+        video_koko = polku_local + video_nimi + video_form                    #Muodostetaan videon koko polku uudestaan
         #print video_nimi
-    return video_nimi
+    return video_nimi                                                         #palautetaan videon nimi
         
 video_nimi = nimiMuunnin(video_nro, video_koko, video_nimi, video_form, polku_local)
 
@@ -56,7 +56,7 @@ db = MySQLdb.connect("stulinux52.ipt.oamk.fi", "ubuntu", "antenni2", "GeoPark")
 
 cursor = db.cursor()
 sql = "insert into Videot(Polku, Nimi, Aikaleima) values('%s', '%s', '%s');" % ((polku_server + video_nimi + video_form), (video_nimi + video_form), testi4)
-print sql
+#print sql
 cursor.execute(sql)
 db.commit()
 db.close()
