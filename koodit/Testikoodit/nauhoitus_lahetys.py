7d961319816d932dfc535f36b0dc962f5202a435
SusiKun <susikun@icloud.com>
2019-12-02 11:39:30 +0200

    nauhoitus_lahetys.py, muokattu tallennuspolku

--- koodit/Testikoodit/nauhoitus_lahetys.py
+++ koodit/Testikoodit/nauhoitus_lahetys.py
@@ -2,19 +2,19 @@ import picamera
 import pysftp
 import MySQLdb
 import os.path
+import time
 from subprocess import call
 
 myHostname = "172.20.240.52"
 myUsername = "ubuntu"
 myPassword = "antenni2"
 
-polku_local = "/home/pi/GeoPark-laite/koodit/Testikoodit/"
+polku_local = "/home/pi/Videos/"
 polku_server = "/home/ubuntu/pi_video/"
 video_nro = 1
 video_nimi = "my_video%d" %(video_nro)
 video_form = ".mp4"
 video_koko = polku_local + video_nimi + video_form
-testi4 = "30"
 
 def nimiMuunnin (video_nro, video_koko, video_nimi, video_form, polku_local):
     while os.path.isfile(video_koko):                                                   
@@ -28,11 +28,12 @@ video_nimi = nimiMuunnin(video_nro, video_koko, video_nimi, video_form, polku_lo
 
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
-camera.start_recording('%s.h264' %video_nimi)
+camera.start_recording('%s%s.h264' %(polku_local, video_nimi))
 camera.wait_recording(10)
 camera.stop_recording()
+aika = time.strftime("%Y%m%d%H%M%S")
 
-command = "MP4Box -add %s.h264 %s.mp4" %(video_nimi, video_nimi)
+command = "MP4Box -add %s%s.h264 %s%s.mp4" %(polku_local, video_nimi, polku_local, video_nimi)
 call([command], shell=True)
 print("vid conv")
 
@@ -55,7 +56,7 @@ with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword
 db = MySQLdb.connect("stulinux52.ipt.oamk.fi", "ubuntu", "antenni2", "GeoPark")
 
 cursor = db.cursor()
-sql = "insert into Videot(Polku, Nimi, Aikaleima) values('%s', '%s', '%s');" % ((polku_server + video_nimi + video_form), (video_nimi + video_form), testi4)
+sql = "insert into Videot(Polku, Nimi, Aikaleima) values('%s', '%s', '%s');" % ((polku_server + video_nimi + video_form), (video_nimi + video_form), aika)
 print sql
 cursor.execute(sql)
 db.commit()
