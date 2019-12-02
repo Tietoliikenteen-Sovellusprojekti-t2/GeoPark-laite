2a4dfb1ded3f4800576bcd20155928459aead533
SusiKun <susikun@icloud.com>
2019-12-02 11:18:35 +0200

    päivitetyt koodit

--- koodit/Testikoodit/impura.py
+++ koodit/Testikoodit/impura.py
@@ -1,21 +1,24 @@
 import io
 import random
 import picamera
+import keyboard
 
 def motion_detected():
     # Randomly return True (like a fake motion detection routine)
     return random.randint(0, 3) == 0
 
 camera = picamera.PiCamera()
-stream = picamera.PiCameraCircularIO(camera, seconds=10)
+stream = picamera.PiCameraCircularIO(camera, seconds=40)
 camera.start_recording(stream, format='h264')
 try:
     while True:
         camera.wait_recording(1)
-        if motion_detected():
+        if keyboard.read_key() == "q":
+            print "painoit NAPPULAAA :DD"
             # Keep recording for 10 seconds and only then write the
             # stream to disk
             camera.wait_recording(5)
-            stream.copy_to('motion.h264')
+            stream.copy_to('motion.h264', seconds = 10)
+            break
 finally:
     camera.stop_recording()
