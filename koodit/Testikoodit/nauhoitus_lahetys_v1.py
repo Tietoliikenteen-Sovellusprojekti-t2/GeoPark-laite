dbbb7ac22c8f39f175c88e14e9cde2c90c5ceee8
SusiKun <susikun@icloud.com>
2019-12-04 12:35:59 +0200

    Testii

--- koodit/Testikoodit/nauhoitus_lahetys_v1.py
+++ koodit/Testikoodit/nauhoitus_lahetys_v1.py
@@ -25,8 +25,6 @@ def nimiMuunnin (video_nro, video_koko, video_nimi, video_form, polku_local):
         print video_nimi
     return video_nimi                                                         
         
-video_nimi = nimiMuunnin(video_nro, video_koko, video_nimi, video_form, polku_local)
-
 def  kamera ():
     camera = picamera.PiCamera()
     stream = picamera.PiCameraCircularIO(camera, seconds=40)
@@ -44,12 +42,11 @@ def  kamera ():
     finally:
         camera.stop_recording()
 
-
 def kaantaja ():
     command = "MP4Box -add %s%s.h264 %s%s.mp4" %(polku_local, video_nimi, polku_local, video_nimi)
     call([command], shell=True)
     print("vid conv")
-    
+
 def ftp (myHostname, myUsername, myPassword):
     with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:      #ftp-blokki
         print "Connection succesfully stablished ... "
@@ -65,7 +62,6 @@ def ftp (myHostname, myUsername, myPassword):
 
         sftp.put(localFilePath, remoteFilePath)     # connection closed automatically at the end of the with-block
 
-
 def sql (polku_server, video_nimi, video_form, aika):
     db = MySQLdb.connect("stulinux52.ipt.oamk.fi", "ubuntu", "antenni2", "GeoPark")
     cursor = db.cursor()
@@ -74,43 +70,15 @@ def sql (polku_server, video_nimi, video_form, aika):
     cursor.execute(sql)
     db.commit()
     db.close()
-    print "Merkinta lisatty tietokantaan"
+    print "Merkinta lisatty tietokantaan"    
 
-# camera = picamera.PiCamera()
-# camera.resolution = (640, 480)
-# camera.start_recording('%s%s.h264' %(polku_local, video_nimi))
-# camera.wait_recording(10)
-# camera.stop_recording()
+video_nimi = nimiMuunnin(video_nro, video_koko, video_nimi, video_form, polku_local)
+
 kamera()
 aika = time.strftime("%Y%m%d%H%M%S")
 kaantaja()
 ftp(myHostname, myUsername, myPassword)
 sql(polku_server, video_nimi, video_form, aika)
-# command = "MP4Box -add %s%s.h264 %s%s.mp4" %(polku_local, video_nimi, polku_local, video_nimi)
-# call([command], shell=True)
-# print("vid conv")
-
-# with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:      #ftp-blokki
-    # print "Connection succesfully stablished ... "
-    
-    # # Define the file that you want to upload from your local directorty
-    # # or absolute "C:\Users\sdkca\Desktop\TUTORIAL2.txt"
-    # localFilePath = polku_local + video_nimi + video_form
-
-    # # Define the remote path where the file will be uploaded
-    # remoteFilePath = polku_server + video_nimi + video_form
-    # print remoteFilePath
-    # print localFilePath
-
-    # sftp.put(localFilePath, remoteFilePath)     # connection closed automatically at the end of the with-block
- 
 
 
-# db = MySQLdb.connect("stulinux52.ipt.oamk.fi", "ubuntu", "antenni2", "GeoPark")
-# cursor = db.cursor()
-# sql = "insert into Videot(Polku, Nimi, Aikaleima) values('%s', '%s', '%s');" % ((polku_server + video_nimi + video_form), (video_nimi + video_form), aika)
-# print sql
-# cursor.execute(sql)
-# db.commit()
-# db.close()
-# print "Merkinta lisatty tietokantaan"
+
