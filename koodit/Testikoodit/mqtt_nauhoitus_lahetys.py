0715b871ca9b483b85de532c3cb23cf901a59bc0
t7mato01 <t7mato01@students.oamk.fi>
2019-12-09 15:11:16 +0200

    muuttuja1

--- koodit/Testikoodit/mqtt_nauhoitus_lahetys.py
+++ koodit/Testikoodit/mqtt_nauhoitus_lahetys.py
@@ -1,3 +1,4 @@
+import paho.mqtt.client as mqtt
 import picamera
 import pysftp
 import MySQLdb
@@ -20,6 +21,8 @@ video_koko = polku_local + video_nimi + video_form
 camera = picamera.PiCamera()
 stream = picamera.PiCameraCircularIO(camera, seconds=40)
 
+muuttuja1 = 0
+
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
 	client.subscribe("test")
@@ -27,76 +30,88 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
 	print(msg.topic+" "+str(msg.payload))
-    def nimiMuunnin (video_nimi, video_koko, video_nro, video_form):
-        while os.path.isfile(video_koko):                                                   
-            video_nimi = "my_video%d" %(video_nro)                                
-            video_nro += 1                                                        
-            video_koko = polku_local + video_nimi + video_form                    
-            print video_nimi
-        return video_nimi                                                         
-            
-    def  kamera ():
-        camera.start_recording(stream, format='h264')
-        try:
-            while True:
-                camera.wait_recording(1)
-                #if keyboard.read_key() == "k":
-                if msg.payload == "vstop":
-                    print "Received VIDEO STOP message!"
-                    camera.stop_recording()
-                    exit()
-                else:
-                    #if keyboard.read_key() == "q":
-                    if msg.payload == "vsave":
-                        print "painoit NAPPULAAA :DD"
-                        # Keep recording for 10 seconds and only then write the
-                        # stream to disk
-                        camera.wait_recording(5)
-                        stream.copy_to('%s%s.h264' %(polku_local, video_nimi), seconds = 10)
-                        break
-        finally:
-            camera.stop_recording()
+    global muuttuja1
+	if msg.payload == "on":
+		print "Received PARKING message!"
+		#os.system("python /home/pi/GeoPark-laite/koodit/Testikoodit/TiedonLisaysTietokantaan_v1.py")
+        muuttuja1 = 1
+        print muuttuja1
+	if msg.payload == "vstart":
+		print "Received VIDEO START message!"
+		os.system("sudo python /home/pi/GeoPark-laite/koodit/Testikoodit/nauhoitus_lahetys_v1.py")
+	if msg.payload == "vstop":
+		keyboard.write('k')
+		print "Received VIDEO STOP message!"
+	if msg.payload == "vsave":
+		print "Received VIDEO SAVE message!"
+		keyboard.write('ctrl+c')
 
-    def kaantaja ():
-        command = "MP4Box -add %s%s.h264 %s%s.mp4" %(polku_local, video_nimi, polku_local, video_nimi)
-        call([command], shell=True)
-        print("vid conv")
+def nimiMuunnin (video_nimi, video_koko, video_nro, video_form):
+    while os.path.isfile(video_koko):                                                   
+        video_nimi = "my_video%d" %(video_nro)                                
+        video_nro += 1                                                        
+        video_koko = polku_local + video_nimi + video_form                    
+        print video_nimi
+    return video_nimi                                                         
+        
+def  kamera ():
+    camera.start_recording(stream, format='h264')
+    try:
+        while True:
+            camera.wait_recording(1)
+            if keyboard.read_key() == "k":
+		camera.stop_recording()
+                exit()
+            else:
+                if muuttuja == 1:
+                    print "painoit NAPPULAAA :DD"
+                    # Keep recording for 10 seconds and only then write the
+                    # stream to disk
+                    camera.wait_recording(5)
+                    stream.copy_to('%s%s.h264' %(polku_local, video_nimi), seconds = 10)
+                    break
+    finally:
+        camera.stop_recording()
 
-    def ftp (myHostname, myUsername, myPassword):
-        with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:      #ftp-blokki
-            print "Connection succesfully stablished ... "
-            
-            # Define the file that you want to upload from your local directorty
-            # or absolute "C:\Users\sdkca\Desktop\TUTORIAL2.txt"
-            localFilePath = polku_local + video_nimi + video_form
+def kaantaja ():
+    command = "MP4Box -add %s%s.h264 %s%s.mp4" %(polku_local, video_nimi, polku_local, video_nimi)
+    call([command], shell=True)
+    print("vid conv")
 
-            # Define the remote path where the file will be uploaded
-            remoteFilePath = polku_server + video_nimi + video_form
-            print remoteFilePath
-            print localFilePath
+def ftp (myHostname, myUsername, myPassword):
+    with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:      #ftp-blokki
+        print "Connection succesfully stablished ... "
+        
+        # Define the file that you want to upload from your local directorty
+        # or absolute "C:\Users\sdkca\Desktop\TUTORIAL2.txt"
+        localFilePath = polku_local + video_nimi + video_form
 
-            sftp.put(localFilePath, remoteFilePath)     # connection closed automatically at the end of the with-block
+        # Define the remote path where the file will be uploaded
+        remoteFilePath = polku_server + video_nimi + video_form
+        print remoteFilePath
+        print localFilePath
 
-    def sql (polku_server, video_nimi, video_form, aika):
-        db = MySQLdb.connect("stulinux52.ipt.oamk.fi", "ubuntu", "antenni2", "GeoPark")
-        cursor = db.cursor()
-        sql = "insert into Videot(Polku, Nimi, Aikaleima) values('%s', '%s', '%s');" % ((polku_server + video_nimi + video_form), (video_nimi + video_form), aika)
-        print sql
-        cursor.execute(sql)
-        db.commit()
-        db.close()
-        print "Merkinta lisatty tietokantaan"    
+        sftp.put(localFilePath, remoteFilePath)     # connection closed automatically at the end of the with-block
 
-    while True:
-        video_nimi = nimiMuunnin(video_nimi, video_koko, video_nro, video_form)
-        kamera()
-        aika = time.strftime("%Y%m%d%H%M%S")
-        kaantaja()
-        ftp(myHostname, myUsername, myPassword)
-        sql(polku_server, video_nimi, video_form, aika)
+def sql (polku_server, video_nimi, video_form, aika):
+    db = MySQLdb.connect("stulinux52.ipt.oamk.fi", "ubuntu", "antenni2", "GeoPark")
+    cursor = db.cursor()
+    sql = "insert into Videot(Polku, Nimi, Aikaleima) values('%s', '%s', '%s');" % ((polku_server + video_nimi + video_form), (video_nimi + video_form), aika)
+    print sql
+    cursor.execute(sql)
+    db.commit()
+    db.close()
+    print "Merkinta lisatty tietokantaan"    
+
+while True:
+    video_nimi = nimiMuunnin(video_nimi, video_koko, video_nro, video_form)
+    kamera()
+    aika = time.strftime("%Y%m%d%H%M%S")
+    kaantaja()
+    ftp(myHostname, myUsername, myPassword)
+    sql(polku_server, video_nimi, video_form, aika)
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect("172.20.240.52")
 client.loop_forever()
-
